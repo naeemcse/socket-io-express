466a45922915e143e12d0647fa88ab0a6f496477
Najmul Islam Naeem <csenajmulislamnaeem@gmail.com>
2024-06-03 12:31:08 +0600

    namespace is leanded

--- socket-app/index.js
+++ socket-app/index.js
@@ -6,28 +6,55 @@ const expressServer = http.createServer(app) ;
 const { Server } = require("socket.io");
 const io= new Server(expressServer);
 
-io.on('connection', (socket) => {
+    io.on('connection', (socket) => {
 
     console.log("Socket is connected for a user!");
     socket.on('disconnect', () => {
         console.log("one user is disconnected!");
     })
     // set time out
-
-    // setTimeout(function (){
-    //     socket.send('Data comes from Socket Backend');
-    // },10000)
+/*
+  setTimeout(function (){
+        socket.send('Data comes from Socket Backend');
+   },10000)*/
 
 // setInerval
-    setInterval(function (){
+  /*  setInterval(function (){
         let date = new Date();
         let hours = date.getHours();
         let minutes = date.getMinutes();
         let seconds = date.getSeconds();
       socket.send(hours + ":" + minutes + ":" + seconds);
-    },2000)
+    },2000)*/
+
+    // Custom event
+    setInterval(function (){
+        let date = new Date();
+        let hours = date.getHours();
+        let minutes = date.getMinutes();
+        let seconds = date.getSeconds();
+        socket.emit( "myEvent",hours + ":" + minutes + ":" + seconds);
+    },100)
+
+
+
+//    Received data from client
+    socket.on("message", function(msg){
+        console.log(msg);
+    })
+    // Custom event
+    socket.on("customEvent", function(msg){
+        console.log( "custm event"+msg);
+    })
 
 })
+
+
+//     Using NameSpace for separate connection
+let buyNsp = io.of("/buy");
+    buyNsp.on('connection', (socket) => {
+        buyNsp.emit("myEvent1","This is Buy NameSpace");
+    })
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
